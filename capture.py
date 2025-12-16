1153b54314a7141644d3fc88347034b0c997ae7d
greycatproduction <greycatproductionsoriginal@gmail.com>
2025-12-16 17:04:51 +0100

    testing

--- capture.py
+++ capture.py
@@ -1,19 +1,7 @@
-from picamera2 import Picamera2
-import cv2
-
+import time
+from picamera2 import Picamera2, Preview
 picam2 = Picamera2()
-config = picam2.create_preview_configuration(main={"size": (1280, 720), "format": "RGB888"})
-picam2.configure(config)
+picam2.start_preview(Preview.QT)
 picam2.start()
-
-while True:
-    frame = picam2.capture_array()  # HxWx3 RGB
-    # OpenCV expects BGR, so convert:
-    frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-    cv2.imshow("cam", frame_bgr)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
-picam2.stop()
-cv2.destroyAllWindows()
+time.sleep(5)
+picam2.capture_file("test.jpg")
