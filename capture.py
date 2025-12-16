6bceff48129cf972785ab88843194490740ffd43
greycatproduction <greycatproductionsoriginal@gmail.com>
2025-12-16 16:56:10 +0100

    prints

--- capture.py
+++ capture.py
@@ -1,12 +1,20 @@
+import os
 from time import sleep
 from picamera2 import Picamera2
 
-def capture():
-    camera = Picamera2()
-    camera.resolution = (640, 480)
-    camera.start_preview()
+print("Saving into:", os.getcwd())
 
-    sleep(2)
-    camera.capture('foo.jpg')
-    
-capture()
+print("Making object")
+cam = Picamera2()
+print("Configuring")
+cam.configure(cam.create_still_configuration(main={"size": (640, 480)}))
+print("Starting cam pipe")
+cam.start()
+print("Sleeping")
+sleep(2)  
+print("Capturing")
+cam.capture_file("foo.jpg")
+print("Stopping")
+cam.stop()
+
+print("Done: foo.jpg")
