38ddd3da87f5e7d7075704ec0ea404faf31ee7f4
greycatproduction <greycatproductionsoriginal@gmail.com>
2025-12-17 12:57:45 +0100

    expanded with classifier

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import cv2
 from picamera2 import Picamera2
 
+classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
 picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration(main={"size": (320, 240), "format": "RGB888"}))
 picam2.start()
 
 while True:
-    frame = picam2.capture_array()          # RGB
+    frame = picam2.capture_array()          
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
     cv2.imshow("cam", frame)
