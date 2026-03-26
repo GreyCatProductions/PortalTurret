4119579c2e0a8407c304518d82117ab7d945e27f
GreyCat <GreyCatProductionsOriginal@gmail.com>
2026-03-26 16:04:32 +0100

    added simply test for servo class

--- Test.py
+++ Test.py
@@ -6,9 +6,16 @@ GPIO.setmode(GPIO.BCM)
 
 servo = MG99RServo(pin=18, start_angle=0, max_deg_per_sec=30)
 
-time.sleep(1)
-servo.set_target(180)
-time.sleep(8)
+travel_time = 180 / 30 + 0.5  # degrees / deg_per_sec + small buffer
+
+try:
+    while True:
+        servo.set_target(180)
+        time.sleep(travel_time)
+        servo.set_target(0)
+        time.sleep(travel_time)
+except KeyboardInterrupt:
+    pass
 
 servo.close()
 GPIO.cleanup()
